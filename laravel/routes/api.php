1b268e188f82c39b89a378b7de4dd1f923d1d805
ipz243kmo <ipz243_kmo@student.ztu.edu.ua>
2026-06-26 11:12:28 +0300

    Update api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\WarehouseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [WarehouseController::class, 'index']);
+Route::get('/products/{id}', [WarehouseController::class, 'show']);
 Route::post('/products', [WarehouseController::class, 'store']);
 Route::patch('/products/{id}', [WarehouseController::class, 'update']);
 Route::delete('/products/{id}', [WarehouseController::class, 'destroy']);
